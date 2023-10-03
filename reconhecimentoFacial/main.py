43597bc66acc70c79b1269cf65d10e0172b216a8
Murilo Preto <87781027+murilo-preto@users.noreply.github.com>
2023-10-03 16:00:06 -0300

    Update main.py

--- reconhecimentoFacial/main.py
+++ reconhecimentoFacial/main.py
@@ -5,50 +5,57 @@ import subprocess
 import sys
 import uuid
 
-# MacAddress do servidor
-serverMacAddress = "ff:ff:ff:ff:ff:ff"
+# Server MAC Address
+SERVER_MAC_ADDRESS = "ff:ff:ff:ff:ff:ff"
 
-# Obtem automaticamente MacAddress do dispositivo
-localMacAddress = ":".join(["{:02x}".format(
+# Obtain the device's MAC Address automatically
+local_mac_address = ":".join(["{:02x}".format(
     (uuid.getnode() >> ele) & 0xFF) for ele in range(0, 8 * 6, 8)][::-1])
-info_dict = {"macaddress": localMacAddress}
+info_dict = {"macaddress": local_mac_address}
 
-# Obtem IP local do servidor a partir do MacAddress
-print(f'Tentando obter IP do servidor via ARP, usando MacAddress:{serverMacAddress}.\n')
-cmd = f'arp -a | findstr "{serverMacAddress.replace(":", "-")}" '
-try:
-    returned_output = subprocess.check_output(
-        cmd, shell=True, stderr=subprocess.STDOUT)
-    parse = str(returned_output).split(' ', 1)
-    serverIp = (parse[1].split(' '))[1]
-    print(f"Utilizando IP local do servidor: {serverIp}.")
-except Exception as e:
-    print(
-        f"Não foi possível obter o endereço de IP do servidor.\nErro:{e}\n")
-    sys.exit(1)
+# Get the local server IP address from the MAC Address
+if SERVER_MAC_ADDRESS != "ff:ff:ff:ff:ff:ff":
+    print(f'Trying to obtain server IP via ARP, using MAC Address: {SERVER_MAC_ADDRESS}.\n')
+    cmd = f'arp -a | findstr "{SERVER_MAC_ADDRESS.replace(":", "-")}" '
+    try:
+        returned_output = subprocess.check_output(
+            cmd, shell=True, stderr=subprocess.STDOUT)
+        parse = str(returned_output).split(' ', 1)
+        server_ip = (parse[1].split(' '))[1]
+        print(f"Using local server IP: {server_ip}.")
+    except Exception as e:
+        print(
+            f"Unable to obtain the server's IP address.\nError: {e}\n")
+        sys.exit(1)
+else:
+    print("Assuming server as localhost.")
+    server_ip = 'localhost'
 
-# Inicia detecção
-print('Iniciando modelos de detecção, pode demorar alguns minutos.')
+# Initialize detection models
+print('Initializing detection models, this may take a few minutes.')
 opencv_path = "haarcascade_frontalface_default.xml"
-opencv_modelo = cv.CascadeClassifier(opencv_path)
+opencv_model = cv.CascadeClassifier(opencv_path)
 rmn = RMN()
 webcam = cv.VideoCapture(0)
 
 while True:
-    ret, imagem_colorida = webcam.read()
-    imagem_cinza = cv.cvtColor(imagem_colorida, cv.COLOR_BGR2GRAY)
+    ret, color_image = webcam.read()
+    gray_image = cv.cvtColor(color_image, cv.COLOR_BGR2GRAY)
     try:
-        faces = opencv_modelo.detectMultiScale(
-            imagem_cinza, scaleFactor=1.1, minNeighbors=4)
-        for (x, y, w, h) in faces:  # Recortar imagens detectadas
-            imagem_recortada = imagem_colorida[y:y+h, x:x+w]
-            print('Face detectada')
+        # Detect faces
+        faces = opencv_model.detectMultiScale(
+            gray_image, scaleFactor=1.1, minNeighbors=4)
+        for (x, y, w, h) in faces:
+            # Crop detected faces
+            cropped_image = color_image[y:y+h, x:x+w]
+            print('Face detected')
 
-            detectedFex = rmn.detect_emotion_for_single_face_image(imagem_recortada)
-            info_dict["fex"] = detectedFex[0]
+            # Detect emotion
+            detected_emotion = rmn.detect_emotion_for_single_face_image(cropped_image)
+            info_dict["fex"] = detected_emotion[0]
 
-            # Envia dados ao servidor
-            r = requests.post(f'http://{serverIp}:3000/api/facialexpressions', json=info_dict)
+            # Send data to the server
+            r = requests.post(f'http://{server_ip}:3000/api/facialexpressions', json=info_dict)
             print(f"{info_dict} : {r.status_code}")
 
     except Exception as e:
